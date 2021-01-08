ec81e5c0d4b334ea19e78289f6b3b6c09f1f1a1e
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-01-08 11:15:03 +0300

    ya_cloud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,20 @@
 
 class UsersController < ApplicationController
   protect_from_forgery with: :null_session
+  before_action :authenticate_user!
 
   def index
-    # render status: :ok, json: User.all.to_json
+    render status: :ok, json: User.all.to_json
   end
 
-  def show; end
+  def show
+    render status: :ok, json: current_user.all.to_json
+  end
 
   private
+
+  def current_user
+
+  end
 
 end
